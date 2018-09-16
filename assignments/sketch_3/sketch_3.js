f3457eefb4f1ba5b5965ec20ed6c68181cea77a7
Sammy <sammyraesylv@gmail.com>
2018-09-16 15:11:04 -0400

    importing an img
    trying to get an imag to rotate around a center point like the square in the example. Im unsure of how to do that.

--- assignments/sketch_3/sketch_3.js
+++ assignments/sketch_3/sketch_3.js
@@ -1,15 +1,22 @@
 // sketch 3
 //moving objects without mousex and mousey
 //example given:
+
+var img;
+function preload() {
+  img = loadImage('data/img.jpg');
+}
+
 function setup(){
-  createCanvas(500, 500);
+  //createCanvas(500, 500
+    image(img, 0, 0);
 }
 function draw() {
   background(200);
-  rectMode(CENTER);
-  //bezierMode(CENTER);//trying to get the bezier curves to rotate
-  translate(width / 2, height / 2);
-  translate(p5.Vector.fromAngle(millis() / 1000, 40));
-  rect(0,0 20,20);
-  //bezier(100, 200, 50,random(height), random(width), random(height));
+  // rectMode(CENTER);
+  // //bezierMode(CENTER);//trying to get the bezier curves to rotate
+  // translate(width / 2, height / 2);
+  // translate(p5.Vector.fromAngle(millis() / 1000, 40));
+  // rect(0,0 20,20);
+  // //bezier(100, 200, 50,random(height), random(width), random(height));
 }
