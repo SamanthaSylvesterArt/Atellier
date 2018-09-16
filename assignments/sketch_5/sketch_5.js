5335b96b963a33552b3b412c1b2ffe2c9d1ba876
Sammy <sammyraesylv@gmail.com>
2018-09-16 17:28:35 -0400

    ball bounce
    rearranging variables because p5 web editor told me to. unclear still on floats and integers.

--- assignments/sketch_5/sketch_5.js
+++ assignments/sketch_5/sketch_5.js
@@ -1,22 +1,28 @@
 // sketch_5.js
-//ball bounce
+//ball bounce and bounce logic
 var x = '100';
 var y = '210';
-var bounceX = float(x);
-var bounceY = float(y);
+var bounceX;
+var bounceY;
+var plusX;
+var plusY;
+var ballDia;
 
 var ballWidth = 5;
 var xAdd = 1;
 var yAdd = 1;
 
-var plusX = int(xAdd);
-var plusY = int(yAdd);
-var ballDia = int(ballWidth); //dia = diameter\
 
 //this feels like a lot of steps just to get an int or a float value
 
 function setup(){
   createCanvas(800,600);
+  var bounceX = float(x);
+  var bounceY = float(y);
+  var plusX = int(xAdd);
+  var plusY = int(yAdd);
+  var ballDia = int(ballWidth); //dia = diameter\
+
 }
 function draw(){
   background(0);
