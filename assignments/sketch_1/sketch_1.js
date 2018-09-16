6042f2cc357d7b5359eddb610aa81b911cdc56c9
Sammy <sammyraesylv@gmail.com>
2018-09-16 11:53:00 -0400

    Getting text to appear
    using the examples to test the link between my html and js files to make sure it works. source: https://p5js.org/reference/#/p5/text

---
+++ assignments/sketch_1/sketch_1.js
@@ -0,0 +1,17 @@
+// sketch_1.js
+// Samantha sylvester, 3165592
+// goal: to create cool looking texts and fonts
+
+function setup(){
+    createCanvas(720,480);
+}
+
+function draw(){
+    background(255);
+    textSize(32);
+    text('word', 10, 30);
+    fill(0, 102, 153);
+    text('word', 10, 60);
+    fill(0, 102, 153, 51);
+    text('word', 10, 90);
+}
