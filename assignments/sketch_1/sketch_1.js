825fb2b54b5d3b558325fcfa7881c4e95c5e76e4
Sammy <sammyraesylv@gmail.com>
2018-09-16 12:05:50 -0400

    importing a font
    Having issues importing a font. I dont understand the parameters. I keep getting a syntax error.

--- assignments/sketch_1/sketch_1.js
+++ assignments/sketch_1/sketch_1.js
@@ -2,16 +2,26 @@
 // Samantha sylvester, 3165592
 // goal: to create cool looking texts and fonts
 
+import p5.font; //import font class??
+
 function setup(){
     createCanvas(720,480);
+    loadFont('data/Adventure.otf', drawText);
+    //font has to be in same folder.... took me awhile to figure that out
 }
 
 function draw(){
     background(255);
-    textSize(32);
-    text('word', 10, 30);
-    fill(0, 102, 153);
-    text('word', 10, 60);
-    fill(0, 102, 153, 51);
-    text('word', 10, 90);
+    // textSize(32);
+    // text('word', 10, 30);
+    // fill(0, 102, 153);
+    // text('word', 10, 60);
+    // fill(0, 102, 153, 51);
+    // text('word', 10, 90);
+}
+
+function drawText(font) {
+  fill('#ED225D');
+  textFont(font, 36);
+  text('p5*js', 10, 50);
 }
